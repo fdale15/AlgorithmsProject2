13db6315fec7410365361b65bab5c2cdc61b8fcf
fdale15 <forrest436@gmail.com>
2016-10-22 11:15:11 -0500

    Reformatted. Added MatchAll to capture multiple regex matches.

--- AlgorithmsProject2/AlgorithmsProject2/Utilities.h
+++ AlgorithmsProject2/AlgorithmsProject2/Utilities.h
@@ -3,37 +3,66 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <regex>
 
 using namespace std;
 
 namespace Utilities
 {
-    static vector<string> ReadFile(string fileName)
-    {
-      auto result = vector<string>();
+	static vector<string> ReadFile(string fileName)
+	{
+		auto result = vector<string>();
 
-      ifstream file(fileName);
+		ifstream file(fileName);
 
-      if (!file.fail())
-      {
-          while (!file.eof())
-          {
-            string s = "";
-            getline(file, s);
-            result.push_back(s);
-          }
-      }
+		if (!file.fail())
+		{
+			while (!file.eof())
+			{
+				string s = "";
+				getline(file, s);
+				result.push_back(s);
+			}
+		}
 
-      return result;
-    }
+		return result;
+	}
 
-    template <typename T>
-    static void PrintVector(vector<T> v)
-    {
-      for (const auto& item : v)
-      {
-          cout << item << endl;
-          //cout << item << endl;
-      }
-    }
+	template <typename T>
+	static void PrintVector(shared_ptr<vector<T>> vp, string prePrint = "", string postPrint = "")
+	{
+		auto copy = *(vp.get());
+
+		for (const auto& item : copy)
+		{
+			cout << prePrint << item << postPrint << endl;
+			//cout << item << endl;
+		}
+	}
+
+	static vector<string> MatchAll(string line, regex exp)
+	{
+		auto result = vector<string>();
+		smatch matches;
+
+		auto startPoint = line.cbegin();
+		while (startPoint != line.cend())
+		{
+			regex_search(startPoint, line.cend(), matches, exp);
+			if (matches.size() > 0)
+			{
+				for (auto i : matches)
+				{
+					result.push_back(i.str());
+					startPoint = i.second;
+				}
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		return result;
+	}
 }
